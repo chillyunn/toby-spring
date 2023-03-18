2ebe202aaf60e0bc16c932abf5e1cf534042865a
giyun <dkwhkp0@naver.com>
2023-03-19 08:18:38 +0900

    템플릿 메소드 패턴과 팩토리 메소드 패턴 적용
    *단점
    1.커넥션 객체를 가져오는 방법의 분리를 위한 상속구조의 사용으로 인해 추후 다른 목적으로 UserDao에 상속을 적용하기 힘듦.
    2.서브클래스는 슈퍼클래스의 기능을 직접 사용할 수 있다. 그래서 슈퍼클래스 내부의 변경이 있을 때 모든 서브클래스를 함께 수정하거나 다시 개발해야할 수 있다.
    3. DB 커넥션 생성 코드를 다른 DAO 클래스에 적용할 수 없다. UserDao 외의 DAO 클래스들이 계속 만들어진다면 매 DAO 클래스마다 중복된 getConnection() 구현 필요
    
    디자인 패턴: 소프트웨어 설계 시 특정 상황에서 자주 만나는 문제를 해결하기 위해 사용할 수 있는 재사용 가능한 솔루션.
    
    템플릿 메소드 패턴: 상속을 통해 슈퍼클래스의 기능을 확장할 때 사용하는 가장 대표적인 방법. 변하지 않는 기능은 슈퍼클래스에 만들어두고 자주 변경되며 확장할 기능은 서브클래스에서 만들도록 한다.
    UserDao의 getConnection() 메소드는 Connection 타입 오브젝트를 생성한다는 기능을 정의해놓은 추상 메소드다.
    
    팩토리 메소드 패턴: 상속을 통해 기능을 확장하게 하는 패턴. 슈퍼클래스에서는 서브클래스에서 구현할 메소드를 호출해서 필요한 타입의 오브젝트를 가져와 사용한다. 서브클래스에서 구체적인 오브젝트 생성 방법을 결정하게 하는 것
    UserDao의 서버클래스의 getConnection() 메소드는 어떤 Connection 클래스의 오브젝트를 어떻게 생성할 것인지를 결정하게 한다.

--- Chapter1/src/main/java/com/toby/dao/UserDao.java
+++ Chapter1/src/main/java/com/toby/dao/UserDao.java
@@ -4,7 +4,7 @@ import com.toby.domain.User;
 
 import java.sql.*;
 
-public class UserDao {
+public abstract class UserDao {
     public void add(User user) throws ClassNotFoundException, SQLException{
         Connection c = getConnection();
         PreparedStatement ps = c.prepareStatement(
@@ -37,10 +37,5 @@ public class UserDao {
 
         return user;
     }
-    private static Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection(
-                "jdbc:mysql://localhost/toby","root","1234");
-        return c;
-    }
+    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
 }
