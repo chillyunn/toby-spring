95bf60cdd61bb5309952fdca0bff503ef3f7ed7c
giyun <dkwhkp0@naver.com>
2023-03-19 08:24:19 +0900

    템플릿 메소드 패턴과 팩토리 메소드 패턴
    디자인 패턴: 소프트웨어 설계 시 특정 상황에서 자주 만나는 문제를 해결하기 위해 사용할 수 있는 재사용 가능한 솔루션.
    
    템플릿 메소드 패턴: 상속을 통해 슈퍼클래스의 기능을 확장할 때 사용하는 가장 대표적인 방법. 변하지 않는 기능은 슈퍼클래스에 만들어두고 자주 변경되며 확장할 기능은 서브클래스에서 만들도록 한다.
    UserDao의 getConnection() 메소드는 Connection 타입 오브젝트를 생성한다는 기능을 정의해놓은 추상 메소드다.
    
    팩토리 메소드 패턴: 상속을 통해 기능을 확장하게 하는 패턴. 슈퍼클래스에서는 서브클래스에서 구현할 메소드를 호출해서 필요한 타입의 오브젝트를 가져와 사용한다. 서브클래스에서 구체적인 오브젝트 생성 방법을 결정하게 하는 것
    UserDao의 서버클래스의 getConnection() 메소드는 어떤 Connection 클래스의 오브젝트를 어떻게 생성할 것인지를 결정하게 한다.

--- Chapter1/src/main/java/com/toby/Main.java
+++ Chapter1/src/main/java/com/toby/Main.java
@@ -1,5 +1,6 @@
 package com.toby;
 
+import com.toby.dao.NUserDao;
 import com.toby.dao.UserDao;
 import com.toby.domain.User;
 
@@ -7,7 +8,7 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        UserDao dao = new UserDao();
+        UserDao dao = new NUserDao();
 
         User user = new User();
         user.setId("giyun");
